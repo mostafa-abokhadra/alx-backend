2efc09f8f828458a18cee31a60e9006cfcd7b71e
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-21 08:14:20 +0300

    LIFO cahching task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -7,6 +7,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 class LIFOCache(BaseCaching):
     """FiFO cache class
     """
+    last_modified = ''
 
     def __init__(self):
         """class constructor
@@ -16,13 +17,18 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """ adding to the cache system
         """
+        last_modified = ''
         if key is None or item is None:
             return
         self.cache_data[key] = item
+        if len(self.cache_data) == super().MAX_ITEMS:
+            LIFOCache.last_modified = key
+
         if len(self.cache_data) > super().MAX_ITEMS:
-            deleted = list(self.cache_data.keys())[-2]
-            del self.cache_data[deleted]
-            print(f"DISCARD: {deleted}")
+            # deleted = list(self.cache_data.keys())[-2]
+            del self.cache_data[LIFOCache.last_modified]
+            print(f"DISCARD: {LIFOCache.last_modified}")
+            LIFOCache.last_modified = key
 
     def get(self, key):
         """getting an item from the cached system
