021e99e99c25533136bf07e2bfdcacd77f5941a1
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-22 01:28:14 +0300

    finally task4 lru done

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,11 +8,12 @@ class LRUCache(BaseCaching):
     """lru class
     """
     lru = []
+
     def __init__(self):
         """initializer
         """
         super().__init__()
-    
+
     def put(self, key, item):
         """adding to the cache
         """
@@ -24,23 +25,31 @@ class LRUCache(BaseCaching):
             if not LRUCache.lru:
                 LRUCache.lru.append(key)
             else:
-                LRUCache.lru.append(key)
-                del LRUCache.lru[0]
+                idx = LRUCache.lru.index(key)
+                key = LRUCache.lru[idx]
+                del LRUCache.lru[idx]
+                LRUCache.lru.insert(0, key)
+                # LRUCache.lru.insert(0, key)
+                # del LRUCache.lru[0]
         else:
             LRUCache.lru.insert(0, key)
 
-        if len(self.cache_data) > super().MAX_ITEMS :
+        if len(self.cache_data) > super().MAX_ITEMS:
+            # print("got here")
             deleted = LRUCache.lru.pop()
             print(f"DISCARD: {deleted}")
-            del self.cach_data[LRUCache.lru[-1]]
+            del self.cache_data[deleted]
+            # print("put", LRUCache.lru)
 
     def get(self, key):
         """getting an item from cache
         """
         if key is None or self.cache_data.get(key) is None:
             return None
+        # print("bef", LRUCache.lru)
         idx = LRUCache.lru.index(key)
         key = LRUCache.lru[idx]
         del LRUCache.lru[idx]
         LRUCache.lru.insert(0, key)
+        # print("aft", LRUCache.lru)
         return self.cache_data[key]
