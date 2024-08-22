06ec60aa64353b978b54934dc8a2899dff5abed3
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-22 04:12:01 +0300

    initail algorithm of LFU + LRU

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -7,6 +7,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 class LFUCache(BaseCaching):
     """ the logic is as follow:
     """
+    lfu_lru = {}
 
     def __init__(self):
         """initializer
@@ -18,13 +19,41 @@ class LFUCache(BaseCaching):
         """
         if key is None or item is None:
             return
+        
+        copy = self.cache_data.copy()
         self.cache_data[key] = item
+
+        if len(copy) == len(self.cache_data):
+            LFUCache.lfu_lru[key] = LFUCache.lfu_lru[key] + 1
+            new_dict = dict({key: LFUCache.lfu_lru[key]})
+            del LFUCache.lfu_lru[key]
+            LFUCache.lfu_lru.updata(new_dict)
+        else:
+            LFUCache.lfu_lru.update({key: 1})
+
         if len(self.cache_data) > super().MAX_ITEMS:
-            pass
+            leastFrequent = min(list(LFUCache.lfu_lru.values()))
+            checkSimilarFrequencey = (LFUCache.lfu_lru.values()).count(leastFrequent)
+            if checkSimilarFrequencey == 1:
+                for key in LFUCache.lfu_lru.keys():
+                    if LFUCache.lfu_lru[key] == leastFrequent:
+                        del LFUCache.lfu_lru[key]
+                        del self.cache_data[key]
+                        break
+            else:
+                for k in reversed(LFUCache.lfu_lru):
+                    if k == key:
+                        del self.cache_data[k]
+                        del LFUCache.lfu_lru[k]
+                        break
 
     def get(self, key):
         """getting an item from cache
         """
         if key is None or self.cache_data.get(key) is None:
             return None
+        LFUCache.lfu_lru[key] += 1
+        new_dict = dict({key :LFUCache.lfu_lru[key]})
+        del LFUCache.lfu_lru[key]
+        LFUCache.lfu_lru.update(new_dict)
         return self.cache_data[key]
--- 0x01-caching/test.py
+++ 0x01-caching/test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-listy = []
-listy.append(None)
-if listy:
-    print(listy)
-
-
+dic = [{"key1": "keyOne"}]
+for dict in dic:
+    if "key1" in dict:
+        print("in")
+print(dic[0]['key1'])
